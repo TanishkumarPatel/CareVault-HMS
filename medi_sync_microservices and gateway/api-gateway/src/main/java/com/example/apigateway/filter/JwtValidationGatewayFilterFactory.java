1b5cdd60c1c50d81c97f3736ae6d8431710a1284
Tanish <202301411@dau.ac.in>
2026-06-01 00:47:12 +0530

    login done

---
+++ medi_sync_microservices and gateway/api-gateway/src/main/java/com/example/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,31 @@
+package com.example.apigateway.filter;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient webClient;
+
+    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientbuilder, @Value("${auth.service.url}") String authServiceUrl) {
+        this.webClient = webClientbuilder.baseUrl(authServiceUrl).build();
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String token=exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if(token==null || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+            return webClient.get().uri("/auth/validate").header(HttpHeaders.AUTHORIZATION,token).retrieve().toBodilessEntity().then(chain.filter(exchange));
+        };
+    }
+}
